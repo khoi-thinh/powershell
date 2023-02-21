cf2c169bfa7191d8a8188583c1371a6480bffafa
Khoi Thinh <thinhduckhoi@gmail.com>
2023-02-22 03:26:51 +0900

    Update reboot_azure_vm.ps1

--- reboot_azure_vm.ps1
+++ reboot_azure_vm.ps1
@@ -1,33 +1,31 @@
 # run from Azure Cloud shell
 
 $azure_data = @(
-    [PSCustomObject]@{SubscriptionName='Sub1_name'},
-    [PSCustomObject]@{SubscriptionName='Sub2_name'},
-    [PSCustomObject]@{SubscriptionName='Sub3_name'})
+    [PSCustomObject]@{SubscriptionName='Subscription_A'},
+    [PSCustomObject]@{SubscriptionName='Subscription_B'},
+    [PSCustomObject]@{SubscriptionName='Subscription_C'})
 
 foreach ($data in $azure_data) {
+    #select Azure subscription to work on and get the list of VM
     $SubscriptionName = $data.SubscriptionName
-
-    #select Azure subscription to work on and get the list of VM based on tag 
     Select-AzureRmSubscription -SubscriptionName $SubscriptionName
 
     $server_List = get-azvm | where {($_.Tags['wins_reboot'] -eq "true")}
 
     ForEach ($server in $server_List) {
         $job_progress = Restart-AzVM -ResourceGroupName $server.ResourceGroupName -Name $server.Name -AsJob
+        while ($job_progress.State -eq "Running") {
+        Start-Sleep 1
+        } # while loop
+        Write-Host "$job_progress is completed"
+
+        $job_result = Receive-Job -Job $job_progress 
+
+        $result = [PSCustomObject]@{
+        ServerName       = $server.Name
+        Reboot_Status   = $job_result.Status
+        }
+        $result >> result.txt
     } #for loop
 
-   #check for result of job and output it to a text file
-    while ($job_progress.State -eq "Running") {
-        Start-Sleep 1
-    }
-    Write-Host "$job_progress is completed"
-
-    $job_result = Receive-Job -Job $job_progress 
-
-    $result = [PSCustomObject]@{
-        ServerName       = $server.Name
-        Status   = $job_result.Status
-    }
-    $result >> result.txt
-}
+} # for loop
