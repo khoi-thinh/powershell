c66062d6119e28a095e026ccc034103e9a88f596
Khoi Thinh <thinhduckhoi@gmail.com>
2023-03-01 13:28:14 +0900

    Create get_ip_from_hosts.ps1

---
+++ get_ip_from_hosts.ps1
@@ -0,0 +1,37 @@
+<# Can import function to profile to make it available as cmdlet
+Run the below command and copy the function content into it
+notepad $profile 
+
+Function support pipeline input so the usage can be any of the following
+Get-IPFromHost hostname
+hostname | Get-IPFromHost
+Get-Content hostname_list.txt | Get-IPFromHost
+
+#>
+function Get-IPFromHost {
+    [cmdletbinding()]
+    param (
+    [Parameter(Mandatory,valuefrompipeline)]
+    [string[]]$pc_list
+    )
+
+    begin {}
+
+    process {
+        foreach ($pc in $pc_list) {
+            $props = @{'HostName'=$pc}
+            try {
+                $result = [System.Net.Dns]::GetHostAddresses($pc)
+                $props.add('IP',$result.IPAddressToString)
+            } #try
+        
+            catch {
+                $props.add('IP','Unknown')
+            } #catch
+         New-Object -TypeName PSObject -Property $props
+         } #foreach
+    } #process
+
+    end {}
+
+} #function
