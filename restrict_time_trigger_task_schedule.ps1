bfa33e9933910a9078b7b94d967a7b1b724b9ca2
Khoi Thinh <thinhduckhoi@gmail.com>
2024-01-24 10:54:04 +0900

    Update restrict_time_trigger_task_schedule.ps1

--- restrict_time_trigger_task_schedule.ps1
+++ restrict_time_trigger_task_schedule.ps1
@@ -8,21 +8,23 @@ $tomorrow = $today.AddDays(1)
 $tomorrow_object = Get-Date $tomorrow -UFormat %Y-%m-%d
 
 $start_time = Read-Host "Enter time in format HH:mm (Hour:minute)"
-$start_time_object = Get-Date $start_time
 $min_start_time_object = Get-Date "02:05"
+$max_start_time_object = Get-Date "03:00"
 
-$start_time = "$tomorrow_object $start_time"
-$start_time_trigger = New-ScheduledTaskTrigger -At $start_time -Once
+$pattern = "^(?:[01]?\d|2[0-3])(?::[0-5]\d){1,2}$"
 
-if ($start_time -eq $null) {
-    Write-Host "Invalid format. Try again!"
+if ($start_time -eq "" -or $start_time -notmatch $pattern) {
+    Write-Host "Invalid format. Try again!" -ForegroundColor Yellow
 }
 else {
-    if ($start_time_object -lt $min_start_time_object) {
-        Write-Host "Start time must be after 02:05 AM. Try again!"
+    $start_time_object = Get-Date $start_time
+    if ($start_time_object -lt $min_start_time_object -or $start_time_object -gt $max_start_time_object) {
+        Write-Host "Start time must be between 02:05 and 03:00. Try again!" -ForegroundColor Yellow
     }
     else {
+        $start_time = "$tomorrow_object $start_time"
+        $start_time_trigger = New-ScheduledTaskTrigger -At $start_time -Once
         Set-ScheduledTask -TaskName $task_name -Trigger $start_time_trigger
-        Write-Host "Trigger time for $task_name updated to $start_time"
+        Write-Host "Trigger time for $task_name updated to $start_time" -ForegroundColor Yellow
     }
 }
